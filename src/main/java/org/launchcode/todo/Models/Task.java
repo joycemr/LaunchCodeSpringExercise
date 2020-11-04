48e8c345e3170fe46e5ce934169c946ff3fc8075
Michael <michael.joyce@illiniarts.com>
2020-11-04 17:10:13 -0600

    Added Task POJO and Repository
    1. Verified that the table was built in the database
    2. I put an Id field on Task, doesn't really seem to need it, except for Hibernate
    Bad form to let the middle tier drive the DB design

--- src/main/java/org/launchcode/todo/Models/Task.java
+++ src/main/java/org/launchcode/todo/Models/Task.java
@@ -1,6 +1,33 @@
 package org.launchcode.todo.Models;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "tasks")
 public class Task {
 
-    public Task() {}
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Integer id;
+    private String text;
+
+    @ManyToOne
+    @JoinColumn(name = "todo_item_id", nullable = false)
+    private TodoItem todoItem;
+
 }
